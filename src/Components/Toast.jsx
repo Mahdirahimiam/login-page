892c8cabd5892f04bba75320fb5a6b74e6f367f4
Mahdirahimiam <146871239+Mahdirahimiam@users.noreply.github.com>
2024-07-26 21:37:27 +0330

    create toasify component

---
+++ src/Components/Toast.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect } from 'react'
+import { toast, ToastContainer } from 'react-toastify'
+export default function Toast({message,type}) {
+    useEffect(()=>{
+        if(type!="info"){
+            toast[type](message, {
+                position: "top-center",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "light",
+                });
+        }
+    },[type,message])
+    return (
+        <>
+            <ToastContainer
+                position="top-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+            />
+        </>
+    )
+}
